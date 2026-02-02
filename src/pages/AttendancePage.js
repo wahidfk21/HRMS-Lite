34031454c9e13f7b91d96622d2650ea74ed2df1e
Abdul Wahid Khan <abdulwahidfk21@gmail.com>
2026-02-03 03:00:21 +0530

    Updated HRMS Frontend

--- src/pages/AttendancePage.js
+++ src/pages/AttendancePage.js
@@ -21,6 +21,7 @@ const AttendancePage = () => {
   useEffect(() => {
     fetchEmployees();
     fetchAttendance();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchEmployees = async () => {
